a50c593088126dec373c2cd36cde182a9810808f
Sebistorm <seba7843@stud.kea.dk>
2021-12-04 18:56:17 +0100

    Initial commit

---
+++ js/createCategory.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', init());
+
+function init() {
+  console.log("Create Category")
+}
+
+document.querySelector("#frmCreateCategory").addEventListener("submit", async function (e) {
+  e.preventDefault();
+  console.log(e.target)
+  const createUrl = e.target.action;
+  try {
+    const formData = new FormData(e.target);
+    const plainformdata = Object.fromEntries(formData.entries());
+    const jsonString = JSON.stringify(plainformdata)
+
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: jsonString
+    }
+
+    const response = await fetch(createUrl, fetchOptions);
+    if(!response.ok) {
+      console.log('Error >:(')
+    }
+
+  } catch (error) {
+    alert(error)
+    console.log(error)
+  }
+})
